d82a154394b8811df9f68d89a2d9f9e2a27c9f9b
swati <swatilingshetty@gmail.com>
2024-07-21 14:26:37 +0530

    Add files via upload

---
+++ numbers_cls.py
@@ -0,0 +1,30 @@
+
+#numbers: Any numeric values..
+
+#integers : those numericals values which are positive or negative or zero and without decimal point...
+
+#floating numbers : those numeric values which arre positive or negative or zero and with decimal point
+
+# a=43
+# print(a)
+# print(type(a))
+
+# b=34.8
+# print(b)
+# # print(type(b))
+
+# name = True
+# print(name)
+# print(type(name))
+
+# complex number : a+ib
+
+# a=3+4j
+
+# print(a)
+# print(type(a))
+
+# b = complex(3,4)
+
+# print(b)
+
