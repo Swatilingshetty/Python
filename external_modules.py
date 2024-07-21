d2f634a546da7144124436b79b7c2c2c969efa20
swati <swatilingshetty@gmail.com>
2024-07-21 14:22:30 +0530

    Add files via upload

---
+++ external_modules.py
@@ -0,0 +1,19 @@
+# external_modules:those modules which we download on our requirment
+
+# pip: python package manager..
+   #pip is the only way to download the modules in python..
+
+# pip install package_name-- command to download the module...
+
+# pip install pymongo
+
+# list all the modules which are installed in your systems//
+#   pip list or pip freeze
+
+#  downloading the list of libraries from a text files//
+   # pip install -p requirments.txt
+
+
+# adding all the installed libraries into a text files..
+
+# pip freeze > requirments.txt
