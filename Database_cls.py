844fd7e11b2e6d69c1d85eb75cb27d936424229d
swati <swatilingshetty@gmail.com>
2024-07-21 14:20:31 +0530

    Add files via upload

---
+++ Database_cls.py
@@ -0,0 +1,8 @@
+# 26/04/2024
+
+# Database: its a staorage device for storing the data..
+
+# Structured database or sequencial (sql,postgres,sqlite,oracle etc..): data  will be stored structured format or table as row and column(tabular format)
+
+# unstructured database or Mysql(Mangodb,dynamodb,cassandra): data will be stored as a json format..
+
